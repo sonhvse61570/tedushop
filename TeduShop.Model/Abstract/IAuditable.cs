4b9d9c95294fb7964f223dea581b6930dfd11f94
sonhvse61570 <sonhoangvinhse61570@gmail.com>
2017-09-30 01:23:12 +0700

    Commit Class

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreateDate { set; get; }
+        string CreatedBy { get; set; }
+        DateTime? UpdatedDate { get; set; }
+        string UpdatedBy { get; set; }
+
+        string MetaKeyword { get; set; }
+        string MeteDescription { get; set; }
+        bool Status { get; set; }
+    }
+}
---
+++ TeduShop.Model/Models/Footer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduShop.Model.Models
+{
+    [Table("Footers")]
+    internal class Footer
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { set; get; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
